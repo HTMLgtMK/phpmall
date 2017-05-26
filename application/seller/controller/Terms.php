7b81f40e04be4319ba606a119bf5d3676a6307c5
GT <GT_GameEmail@163.com>
2017-05-26 18:42:18 +0800

    修改使用3.3.7生产版bootstrap

--- application/seller/controller/Terms.php
+++ application/seller/controller/Terms.php
@@ -32,8 +32,41 @@
 	 }
 	 
 	 public function index(){
-		 $seller_id=Session::get('seller.id');
+		 $seller_id=Session::get('seller.id');		 
+		 $term_id=Request::instance()->param('term_id');
+		 $this->terms_path($term_id);
+		 $this->terms_child($term_id);
 		 return $this->fetch();
+	 }
+	 
+	 /**
+	  * 获取该分类下的所有分类
+	  * @param $term_id 分类id
+	  */
+	 private function terms_child($term_id=0){
+		 $terms_child=$this->db
+						->table($this->tb_store_terms)
+						->where('parent_id',$term_id)
+						->select();
+		 $this->assign('terms_child',$terms_child);
+	 }
+	 
+	 /*
+	  * 获取当前分类的路径
+	  */
+	 private function terms_path($term_id=0){
+		 $terms_path=array();
+		 while($term_id!=0){
+			 $term=$this->db
+						->table($this->tb_store_terms)
+						->where('id',$term_id)
+						->find();
+			 $terms_path[]=$term;
+			 $term_id=$term['parent_id'];
+		 }
+		 //反转数组
+		 $terms_path=array_reverse($terms_path,true);
+		 $this->assign('terms_path',$terms_path);
 	 }
  }
 ?>
