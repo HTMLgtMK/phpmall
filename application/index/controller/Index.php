a24c845855408d0be7ab9b46eaec0b1b096b3f48
Aman <zman_2016@163.com>
2017-05-28 10:12:33 +0800

    '我的主页'的编写,以及thinkshop首页部分样式的修改

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,8 +3,13 @@ namespace app\index\controller;
 use \think\Controller;
 
 class Index extends Controller{
-	
+	//thinkshop首页
 	public function index(){
+		return $this->fetch();
+	}
+
+	//我的主页
+	public function main(){
 		return $this->fetch();
 	}
 	
